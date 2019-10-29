1b7313fdc9042255f9606877d4cc1b04f87a4516
rick <15701656560@163.com>
2019-10-29 19:26:45 +0800

    业务

--- app/src/main/java/com/longer/creditManager/buinese/detail/BusineDataAdapter.kt
+++ app/src/main/java/com/longer/creditManager/buinese/detail/BusineDataAdapter.kt
@@ -1,6 +1,5 @@
 package com.longer.creditManager.buinese.detail
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -19,36 +18,36 @@ import hxz.www.commonbase.util.log.LogShow
  **/
 class BusineDataAdapter : BaseAdapter<DataBean>() {
 
-var inflater:LayoutInflater?=null
+    var inflater: LayoutInflater? = null
     var titleList: TitleListBean? = null
-    var tvList= mutableListOf<TextView>()
+    var tvList = mutableListOf<TextView>()
     override fun getItemLayoutResId(data: DataBean?, position: Int) = R.layout.adapter_simple
     override fun bindData(holder: ViewHolder, data: DataBean?, position: Int) {
-        var container=holder.getView<LinearLayout>(R.id.ll_container)
-        if (tvList.isEmpty())
-        {
-            for(i in 0..container.childCount)
-            {
-                tvList.add(container.getChildAt(i) as TextView)
+        var container = holder.getView<LinearLayout>(R.id.ll_container)
+        if (tvList.isEmpty()) {
+            for (i in 0 until container.childCount) {
+                container.getChildAt(i)?.let {
+                    tvList.add(it as TextView)
+                }
             }
 
         }
-        LogShow.i("BusineDataAdapterdapter.kt  bindData", tvList.size,data)
+        LogShow.i("BusineDataAdapterdapter.kt  bindData", container.childCount, tvList.size, data)
 
         if (position == 0) {
             titleList = GsonUtil.fromJson(data?.lilv, TitleListBean::class.java)
             LogShow.i("BusineDataAdapterdapter.kt  bindData list", titleList?.titleList?.size)
-            titleList?.titleList?.forEach {
+            titleList?.titleList?.forEachIndexed { index, titleBean ->
+                run {
+                    tvList.get(index).text = titleBean.fieldNote
+                }
 
             }
 
-        } else {
-
         }
-    }
 
-    private fun createTextview(context: Context, str: String, parent: LinearLayout) {
-         LogShow.i("BusineDataAdapter.kt  createTextview",str)
+//    private fun createTextview(context: Context, str: String, parent: LinearLayout) {
+//        LogShow.i("BusineDataAdapter.kt  createTextview", str)
 //        var tv = TextView(context)
 //        tv.text = str
 //        tv.apply {
@@ -56,11 +55,12 @@ var inflater:LayoutInflater?=null
 //          layoutParams.width = 100F.dp2px()
 //         layoutParams.height = ActionBar.LayoutParams.MATCH_PARENT
 //        }
-        var textview=inflater?.inflate(R.layout.layout_title,null)
-//        tv.setTextColor(R.color.black.getColor())
-//        tv.setTextSize(12F)
-        parent.addView(textview)
+//        var textview=inflater?.inflate(R.layout.layout_title,null)
+////        tv.setTextColor(R.color.black.getColor())
+////        tv.setTextSize(12F)
+//        parent.addView(textview)
+
+//    }
 
     }
-
 }
